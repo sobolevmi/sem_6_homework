cbd7972a1db6b2ef07f615172b3b075721e6e222
Maksim Sobolev <sobmig@gmail.com>
2022-07-14 00:53:42 +0300

    папка перенесена в локальный репозиторий с домашним заданием к семинару номер 6

---
+++ task44/Program.cs
@@ -0,0 +1,25 @@
+﻿int [] CreateArray (int N)
+{
+    int [] massive = new int [N];
+    massive [0] = 0;
+    massive [1] = 1;
+    for (int i = 2; i < massive.Length; i++)
+    {
+        massive [i] = massive [i - 2] + massive [i - 1];
+    }
+    return massive;
+}
+
+void ShowArray (int [] mas)
+{
+    for (int j = 0; j < mas.Length; j++)
+    {
+        Console.Write (mas [j] + " ");
+    }
+}
+
+Console.Write ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+int [] array = CreateArray (number);
+Console.Write ($"{number} -> ");
+ShowArray (array);
